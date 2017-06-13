116c11d111369531ae102890bdf818ddae3e3d5d
Kalvin Krajca <kalman98@kalman98.net>
2017-06-12 20:17:49 -0500

    Initial commit.

---
+++ src/main/java/com/kalman98/elicitation/Elicitation.java
@@ -0,0 +1,35 @@
+package com.kalman98.elicitation;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Elicitation.MODID, version = Elicitation.VERSION)
+public class Elicitation
+{
+	//---------------- Mod Constants ----------------\\
+    public static final String MODID = "elicitation";
+    public static final String VERSION = "0.0.1";
+    
+    
+    //------------- Initialization -------------\\ 
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    	
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    	
+    }
+}
